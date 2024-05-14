9bb83c2c566b10d5fe87f00872b0c73dca90c54e
Filip Srnec <140503581+Jackknife1@users.noreply.github.com>
2024-05-14 21:01:49 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-#Aplikace vytvořená 25.10.2022 - Verze 1.0
-
-#Verze 2.0 - 18.11.2023
-
-#Verze 3.0 - Finální verze - 20.11.2023
-
 from pojistovna import Pojistovna
 from spravce_io import SpravceIO
 
